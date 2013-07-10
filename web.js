4a682ce80374592307e663727dd91d89ad08e666
Andres C Alonso <andrescalonso@gmail.com>
2013-07-10 04:27:26 +0000

    more debugging

--- web.js
+++ web.js
@@ -4,17 +4,19 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var fs = require("fs");
 
-  var greeting = fs.readFile('index.html', function(err, data) {
-    if (err) {
-        var error = "NEW ERROR: " + err;
-        console.log(error);
-        return error;
-    }
-    console.log("*********************************************** NO ERRORS IN FILE READING");
-    console.log(typeof data);
-    return data.toString();  
+  var greeting = function() {
+    var message = '';
+    fs.readFile('index.html', function(err, data) {
+      if (err) {
+        throw error;
+      }
+      console.log("*********************************************** NO ERRORS IN FILE READING");
+      console.log(typeof data);
+      message = data.toString();  
+    });
+    return message();
   });
-  response.send(greeting);
+  response.send(message);
 });
 
 var port = process.env.PORT || 5000;
